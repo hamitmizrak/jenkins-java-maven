b8b16958ab99b4eb5d9c52928ac11cbbf11c3b67
Hamit Mizrak <hamitmizrak@gmail.com>
2024-09-26 14:48:30 +0300

    revize

--- JavaJenkinsCompiler.java
+++ JavaJenkinsCompiler.java
@@ -2,6 +2,6 @@
 // Class
 public class JavaJenkinsCompiler {
     public static void main(String [] args){
-        System.out.println("Jenkins Java Maven")
+        System.out.println("Jenkins Java Maven");
     } //end psvm
 } //end class
